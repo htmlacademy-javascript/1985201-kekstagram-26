43a6da4be4947804d4589b9ee0c5987c2182a9f0
Ирина <irene7494@mail.ru>
2022-06-11 14:49:13 +0300

    Исправляет ошибки по линтеру

--- js/main.js
+++ js/main.js
@@ -1,32 +1,29 @@
 /*При решении задачи использовался ресурс MDN*/
 
 function randomNumbers (min, max) {
-  minRoundCeil = Math.ceil(min);
-  maxRoundCeil = Math.ceil(max);
-  maxRoundFloor = Math.floor(max);
-  let result = Math.floor(Math.random() * (maxRoundFloor - minRoundCeil + 1)) + minRoundCeil;
+  const minRoundCeil = Math.ceil(min);
+  const maxRoundCeil = Math.ceil(max);
+  const maxRoundFloor = Math.floor(max);
+  const result = Math.floor(Math.random() * (maxRoundFloor - minRoundCeil + 1)) + minRoundCeil;
 
   if ((min >= max) || (result < 0) || (minRoundCeil === maxRoundCeil)) {
-    console.log('Функция не может быть выполнена');
+    return false;
   } else {
     return result;
   }
 }
 
-console.log(randomNumbers(4, 10));
+randomNumbers(2.7, 2.9);
 
 /*При решении задачи использовался ресурс MDN*/
 
-let content = 'I Love JS';
-let maxLength = 5;
-
 function calculateMaxLength (content, maxLength) {
-  let result = maxLength - content.length;
+  const result = maxLength - content.length;
   if (result > 0) {
-    console.log ('Длина строки соответствует заданному параметру');
+    return true;
   } else {
-    console.log ('Длина строки не соответствует заданному параметру');
+    return false;
   }
 }
 
-calculateMaxLength (content, maxLength);
+calculateMaxLength ('I Love Js', 10);
