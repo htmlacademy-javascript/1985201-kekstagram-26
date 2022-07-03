57087a974b3e5031ffb7b6b8ecf213a7df018530
Keks <keks@htmlacademy.ru>
2022-07-03 17:32:57 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/render.js
@@ -0,0 +1,21 @@
+import {createPosts} from './data.js';
+
+const usersPosts = document.querySelector('.pictures');
+
+const similarPostTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+
+const similarPosts = createPosts();
+
+const similarListFragment = document.createDocumentFragment();
+
+similarPosts.forEach(({url, comments, likes}) => {
+  const postElement = similarPostTemplate.cloneNode(true);
+  postElement.querySelector('.picture__img').src = url;
+  postElement.querySelector('.picture__comments').textContent = comments.length;
+  postElement.querySelector('.picture__likes').textContent = likes;
+  similarListFragment.appendChild(postElement);
+});
+
+usersPosts.appendChild(similarListFragment);
