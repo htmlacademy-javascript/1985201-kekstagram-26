58e1685e3ada7ebd6e780afd7064c724657f4f70
Ирина <irene7494@mail.ru>
2022-07-09 00:29:42 +0300

    заканчивает задание

--- js/render-big-pic.js
+++ js/render-big-pic.js
@@ -1,8 +1,13 @@
+/*Список констант*/
+
 const bigPicture = document.querySelector('.big-picture');
 const bigPictureImg = bigPicture.querySelector('.big-picture__img').querySelector('img');
 const CloseButton = bigPicture.querySelector('.big-picture__cancel');
 const likesCount = document.querySelector('.likes-count');
 const commentsCount = document.querySelector('.comments-count');
+const socialCaption = document.querySelector('.social__caption');
+const socialComments = document.querySelector('.social__comments');
+const socialCommentTemplate = socialComments.querySelector(':first-child');
 
 /*Открытие*/
 
@@ -15,36 +20,48 @@ function openBigPicture () {
   document.body.classList.add('modal-open');
 }
 
+/*Закрытие (по клику и ESC)*/
+
+/*Закрытие с помощью ESC*/
+
+const pressEsc = document.addEventListener('keydown', (evt) => {
+  if ( evt.key === 'Escape' ) {
+    evt.preventDefault();
+    bigPicture.classList.add('hidden');
+  }
+});
+
+/*Закрытие по клику*/
+
+function closeBigPicture () {
+  bigPicture.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  document.removeEventListener('keydown', pressEsc);
+}
+
+/*Закрытие при нажатии на кнопку*/
+
+CloseButton.addEventListener('click', () => closeBigPicture());
+
 const renderBigPicture = (url, comments, likes, description) => {
 
   openBigPicture();
 
+  const writeComment = (comment) => {
+    const newComment = socialCommentTemplate.cloneNode(true);
+    const commentAvatar = newComment.querySelector('img');
+    commentAvatar.src = comment.avatar;
+    commentAvatar.alt = comment.name;
+    newComment.querySelector('.social__text').textContent = comment.message;
+
+    return newComment;
+  };
+
   bigPictureImg.src = url;
   likesCount.textContent = likes;
   commentsCount.textContent = comments.length;
-
-  /*Закрытие (по клику и ESC)*/
-
-  /*Закрытие с помощью ESC*/
-
-  const pressEsc = document.addEventListener('keydown', (evt) => {
-    if ( evt.key === 'Escape' ) {
-      evt.preventDefault();
-      bigPicture.classList.add('hidden');
-    }
-  });
-
-  /*Закрытие по клику*/
-
-  function closeBigPicture () {
-    bigPicture.classList.add('hidden');
-    document.body.classList.remove('modal-open');
-    document.removeEventListener('keydown', pressEsc);
-  }
-
-  /*Закрытие при нажатии на кнопку*/
-
-  CloseButton.addEventListener('click', () => closeBigPicture());
+  socialCaption.textContent = description;
+  socialComments.replaceChildren(...comments.map(writeComment));
 };
 
 export {renderBigPicture};
